150a142b07ddcd67af5eac8a2cf8c66b1c1e6413
Fippiy <profier4@gmail.com>
2019-01-21 14:39:02 +0900

    Edit login system

--- pictweet/views.py
+++ pictweet/views.py
@@ -18,3 +18,14 @@ def index(request):
 #     msg.save()
 #     return redirect(to="/nekotter")
 #   return render(request, 'nekotter/new.html')
+
+
+
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
+
+class SignUpView(generic.CreateView):
+      form_class = UserCreationForm
+      success_url = reverse_lazy('login')
+      template_name = 'accounts/signup.html'
