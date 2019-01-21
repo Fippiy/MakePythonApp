7dd4970f598523348682e9d68aea968a33a9315f
Fippiy <profier4@gmail.com>
2019-01-21 13:09:17 +0900

    Edit testapp

--- testapp/views.py
+++ testapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import testapp_text
 
@@ -12,3 +12,19 @@ def index(request):
   'lists': text_list,
   }
   return render(request, 'testapp/index.html', context)
+
+def new(request):
+  # msg = request.POST['word']
+  if request.method == 'POST':
+    msg = testapp_text.objects.create(test_text=request.POST['word'])
+    msg.save()
+    return redirect(to="/testapp")
+  # 新規投稿画面をだす
+  # フォームを表示
+  return render(request, 'testapp/new.html')
+
+# def create(request):
+#   # 投稿処理を書く
+#   msg = request.POST['word']
+#   # 終わったら投稿完了ページ
+#   return render(request, 'testapp/new.html', msg)
